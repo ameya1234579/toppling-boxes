c3f48c064baa58af0a3660ea3aa14471eedbdb4a
ameya1234579 <65115166+ameya1234579@users.noreply.github.com>
2020-05-20 18:08:52 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,9 +1,11 @@
 class Box{
     constructor(x,y,width,height){
         var options = {
-            restitution: 0.8
+            restitution: 1.0
         }
-        this.body = Bodies.rectangle(x,y,width,height);
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
         World.add(world,this.body);
     }
     display(){
